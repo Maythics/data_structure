2a3ec6dc9cd221c9a8905c42a1fb22567ff979ce
maythics <maythics@Gargantua>
2024-12-08 20:42:18 +0800

    cuckoo(完成一半)

--- Cuckoo/cuckoo.h
+++ Cuckoo/cuckoo.h
@@ -5,43 +5,9 @@
 
 using namespace std;
 
-
-////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////// UTILITIES /////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
-bool isPrime( int n )
-{
-    if( n == 2 || n == 3 )
-        return true;
-
-    if( n == 1 || n % 2 == 0 )
-        return false;
-
-    for( int i = 3; i * i <= n; i += 2 )
-        if( n % i == 0 )
-            return false;
-
-    return true;
-}
-
-int nextPrime( int n )
-{
-    if( n % 2 == 0 )
-        ++n;
-
-    for( ; !isPrime( n ); n += 2 )
-        ;
-
-    return n;
-}
-////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////// UTILITIES ////////////////////////////////->end
-////////////////////////////////////////////////////////////////////////////////
-
-
-
-
-
+/// 两个工具函数，功用如其名（实现见文件末尾）
+bool isPrime(int n);
+int nextPrime(int n);
 
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////// STRING HASH /////////////////////////////////
@@ -109,57 +75,57 @@ class StringHashFamily
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////// INT HASH ////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
-/// IntHashFamily：
-/// 这个类是用来记录生成针对整型对象的Hash函数的，其功能有：
-/// 1. 生成一个count那么多的Hash函数
-/// 2. 能够调用getNumberOfFunctions返回有几个函数
-/// 3. 能够给传入的int提供一个Hash值
-// class IntHashFamily
-// {
-//   public:
-//     /// 构造函数：
-//     IntHashFamily(int count) : MULTIPLIERS(count)
-//     {
-//         generateNewFunctions();
-//     }
+// / IntHashFamily：
+// / 这个类是用来记录生成针对整型对象的Hash函数的，其功能有：
+// / 1. 生成一个count那么多的Hash函数
+// / 2. 能够调用getNumberOfFunctions返回有几个函数
+// / 3. 能够给传入的int提供一个Hash值
+class IntHashFamily
+{
+  public:
+    /// 构造函数：
+    IntHashFamily(int count) : MULTIPLIERS(count)
+    {
+        generateNewFunctions();
+    }
 
-//     int getNumberOfFunctions() const
-//     {
-//         return MULTIPLIERS.size(); // 返回存着的Hash函数个数
-//     }
+    int getNumberOfFunctions() const
+    {
+        return MULTIPLIERS.size(); // 返回存着的Hash函数个数
+    }
 
-//     void generateNewFunctions()
-//     {
-//         int r = rand()% 600 + 1 ; // 先把一个大小为1到600之间的随机数存在r中
-//         for (auto& mult : MULTIPLIERS)
-//         {
-//           r = nextPrime(r+50);
-//           mult = r;  // 得到一个质数（跨度更大）
-//         }
-//     }
+    void generateNewFunctions()
+    {
+        int r = rand()% 600 + 1 ; // 先把一个大小为1到600之间的随机数存在r中
+        for (auto& mult : MULTIPLIERS)
+        {
+          r = nextPrime(r+50);
+          mult = r;  // 得到一个质数（跨度更大）
+        }
+    }
 
-//     int hash(const int & x, int which) const
-//     {
-//         const int multiplier = MULTIPLIERS[which];
-//         int hashVal = 0;
-//         hashVal = multiplier * hashVal;
-//         return hashVal;
-//     }
+    /// 必须用size_t，用int会有负号，导致得到数组范围外的下标
+    size_t hash(const int & x, int which) const
+    {
+      const int multiplier = MULTIPLIERS[which];
+      size_t hashVal = 0;
+      hashVal = multiplier * x;
+      return hashVal;
+    }
 
-//     // 打印 MULTIPLIERS 的函数
-//     void printMultipliers() const
-//     {
-//         cout << "Multipliers: ";
-//         for (int mult : MULTIPLIERS) {
-//             cout << mult << " ";
-//         }
-//         cout << endl;
-//     }
+    // 打印 MULTIPLIERS 的函数
+    void printMultipliers() const
+    {
+        cout << "Multipliers: ";
+        for (int mult : MULTIPLIERS) {
+            cout << mult << " ";
+        }
+        cout << endl;
+    }
 
-//   private:
-//     vector<int> MULTIPLIERS; // 存储哈希函数的乘数
-// };
-
+  private:
+    vector<int> MULTIPLIERS; // 存储哈希函数的乘数
+};
 
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////// INT HASH ///////////////////////////////->end
@@ -179,12 +145,10 @@ class HashTable
   public:
 
     /// 构造函数如下：
-
     HashTable(int TableSize = 101, int numHashFunctions = 3) : TableSize(TableSize), currentSize(0), numHashFunctions(numHashFunctions), hashFunctions(numHashFunctions)
     {
       /// 直接把numHashFunctions这个值更新为hashFunctions这个实例中MULTIPLIER的大小
       /// numHashFunctions = hashFunctions.getNumberOfFunctions();
-      // rehashes = 0;
       array.resize(TableSize);
       makeEmpty( );
     }
@@ -198,7 +162,7 @@ class HashTable
 
     void print_Table()
     {
-      for( auto & entry : array ) ///如果isActive就打印里面的数字，否则不打印
+      for( auto & entry : array ) ///如果isActive就打印里面的element，否则不打印
         if(entry.isActive == true )
           cout << entry.element << "\t" ;
         else
@@ -227,7 +191,7 @@ class HashTable
       return find_element_pos(x) != -1;
     }
 
-    bool insert(Datatype & x)  /// 将一个数据类型为Datatype的元素x加入到表中
+    bool insert(Datatype & x, bool debug = false)  /// 将一个数据类型为Datatype的元素x加入到表中，debug是调试用的参数，默认为false，即不打印任何结果到终端中，若为true，将会打印详细步骤
     {
       /// 如果有x元素了，直接返回false，不允许有重复元素
       if( contains(x) )
@@ -237,17 +201,17 @@ class HashTable
       //   expand( );
 
       /// 调用private的函数insert_element完成任务
-      return insert_element(x);
+      return insert_element(x, debug);
 
     }
 
-    bool insert(Datatype && x)  /// 将一个数据类型为Datatype的元素x加入到表中（右值版本）
+    bool insert(Datatype && x, bool debug = false)  /// 将一个数据类型为Datatype的元素x加入到表中（右值版本）debug是调试用的参数，默认为false，即不打印任何结果到终端中，若为true，将会打印详细步骤
     {
       /// 如果有x元素了，直接返回false，不允许有重复元素
       if( contains(x) )
         return false;
       /// 调用private的函数insert_element完成任务
-      return insert_element(std::move(x)); /// 转换为右值传入
+      return insert_element(std::move(x),debug); /// 转换为右值传入
 
     }
 
@@ -263,21 +227,16 @@ class HashTable
         return true;
     }
 
-    /// rehash函数，在碰撞次数过多时就会触发了
-    void rehash( int newSize ) /// 传入参数的大小是指新表的大小
+    bool remove( const Datatype && x )
     {
-      cout<< "############# rehash trigger #############" <<endl;
-      hashFunctions.generateNewFunctions();
-      vector<HashEntry> old = array; //std::move(array);
-      array.resize(nextPrime(newSize)); /// 新表，大小为newSize
-      TableSize = array.size();
-      makeEmpty(); /// 清空里面的所有
-      /// 接下来把old中的元素一个个插入进新表中：
-      for( auto & entry : old )
-        if( entry.isActive )
-          insert( std::move(entry.element)); /// 转化为右值，接管
+        int current_pos = find_element_pos(std::move(x));
+        if( current_pos == -1 )
+          return false; /// 发现未使用（即不存在该元素）
+        /// 否则，就是找到了
+        array[current_pos].isActive = false;
+        currentSize -= 1;
+        return true;
     }
-
 
   private:
 
@@ -285,11 +244,12 @@ class HashTable
     {
         Datatype element;
         bool isActive;
-        HashEntry( const Datatype & e = Datatype( ), bool a = false )
-          : element{ e }, isActive{ a } { }
-
-        HashEntry( Datatype && e, bool a = false )
-          : element{ std::move( e ) }, isActive{ a } { }
+        /// 拷贝构造函数
+        HashEntry( const Datatype & element = Datatype(), bool isActive = false)
+          : element{ element }, isActive{ isActive } { }
+        /// 移动构造函数
+        HashEntry( Datatype && element, bool isActive = false )
+          : element{ std::move( element ) }, isActive{ isActive } { }
     };
 
     vector<HashEntry> array; /// 表的本体就是这个array
@@ -298,16 +258,30 @@ class HashTable
     int numHashFunctions; /// 记录这个表有几个Hash函数，其实就从hashFunctions这个实例里得来
     FunctionFamily hashFunctions; /// 根据设定的FunctionFamily得知是什么Hash函数类型，hashFunctions就是对应的那个实例，记录了这个表所用到的Hash函数们
 
-
     /// 下面是一些Private函数
 
-    /// 这是一个辅助函数，返回第i个Hash函数规则下，元素x的位置
+    /// rehash函数，在碰撞次数过多时就会触发了
+    void rehash( int newSize , bool debug) /// 传入参数的大小是指新表的大小
+    {
+      if (debug == true) ///若处于debug模式，则打印出来
+        cout<< "############# rehash trigger #############" <<endl;
+      hashFunctions.generateNewFunctions();
+      vector<HashEntry> old = std::move(array); //std::move(array);
+      array.resize(nextPrime(newSize)); /// 新表，大小为newSize
+      TableSize = array.size();
+      makeEmpty(); /// 清空里面的所有
+      /// 接下来把old中的元素一个个插入进新表中：
+      for( auto & entry : old )
+        if( entry.isActive )
+          insert( std::move(entry.element),debug); /// 转化为右值，接管
+    }
+
+    /// 如下是一个辅助函数，返回第i个Hash函数规则下，元素x的位置
     int find_position(Datatype x, int i) const
     {
       /// 计算规则很简单，只用先把x和i传给hashFunctions对象的hash函数，就会得到hash值，再填入Table中（考虑到实际大小对TableSize取模就行）
       return (hashFunctions.hash(x,i) % TableSize);
     }
-
 
     /// find_element_pos会找传入的元素是否存在，如果真能找到，会返回其在表中的位置
     int find_element_pos( const Datatype & x ) const
@@ -325,12 +299,11 @@ class HashTable
       return -1;
     }
 
-
     /// 插入元素的核心函数，在调用它时，已排除了相同元素的可能
-    bool insert_element(Datatype & x)
+    bool insert_element(Datatype & x, bool debug)
     {
       int KICK = 0 ;
-      int MAX_KICK = max(10,min(currentSize/2,200)); /// 以此设置为最多的碰撞踢除数量
+      int MAX_KICK = 1+min(currentSize/4,99); /// 以此设置为最多的碰撞踢除数量
       while( KICK < MAX_KICK )
       {
         for( int i = 0; i < numHashFunctions; ++i ) /// 逐个尝试该元素x对应的Hash值
@@ -352,17 +325,17 @@ class HashTable
         /// 采用swap的策略，现在x就获得了被踢出来的元素，他将继续寻找下家
         std::swap( x, array[ final_pos ].element );
         KICK+=1;
-
-        cout << "<<<<<<<<<<<<kick!!!<<<<<<<<<<<" << endl;
+        if (debug == true) ///若处于debug模式，则打印出来
+          cout << "<<<<<<<<<<<< kick!!!<<< " << x << " <<<<< "<< endl;
       }
-      rehash(2*TableSize);
-      return insert_element(x);
+      rehash(2*TableSize,debug);
+      return insert_element(x,debug);
     }
 
-    bool insert_element(Datatype && x)
+    bool insert_element(Datatype && x,bool debug)
     {
       int KICK = 0 ;
-      int MAX_KICK = 10;
+      int MAX_KICK = 1+min(currentSize/4,99); /// 以此设置为最多的碰撞踢除数量
       while( KICK < MAX_KICK )
       {
         for( int i = 0; i < numHashFunctions; ++i ) /// 逐个尝试该元素x对应的Hash值
@@ -384,15 +357,46 @@ class HashTable
         /// 采用swap的策略，现在x就获得了被踢出来的元素，他将继续寻找下家
         std::swap( x, array[ final_pos ].element );
         KICK+=1;
-
-        cout << "<<<<<<<<<<<<kick!!!<<<<<<<<<<<" << endl;
+        if (debug == true) ///若处于debug模式，则打印出来
+          cout << "<<<<<<<<<<<< kick!!!<<< " << x << " <<<<< "<< endl;
       }
-      rehash(2*TableSize);
-      return insert_element(std::move(x));
-
+      rehash(2*TableSize,debug);
+      return insert_element(std::move(x),debug);
     }
 
 };
 //////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////// HASH TABLE //////////////////////////////->end
 //////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////// UTILITIES /////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+/// 工具函数，用来辅助Hash映射的生成等
+/// 判断是否是质数：
+bool isPrime(int n)
+{
+    if( n == 2 || n == 3 )
+        return true; /// 2和3直接true
+    if( n == 1 || n % 2 == 0 )
+        return false; /// 直接否决偶数，更快速一些
+    for( int i = 3; i * i <= n; i += 2 )
+        if( n % i == 0 )
+            return false;
+    return true;
+}
+int nextPrime(int n) /// 返回n的紧接质数后元
+{
+    if( n % 2 == 0 )
+        ++n; /// 偶数否决
+    for( ; !isPrime( n ); n += 2 )
+        ; ///奇数递增试凑
+
+    return n;
+}
+////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////// UTILITIES ////////////////////////////////->end
+////////////////////////////////////////////////////////////////////////////////
--- Cuckoo/main.cpp
+++ Cuckoo/main.cpp
@@ -8,8 +8,8 @@ using namespace std;
 
 /////////////////////////////////// STRING HASH TEST ///////////////////////////////////
 // 用于生成足够多的测试字符串
-std::string generateRandomString(int length) {
-    std::string str;
+string generateRandomString(int length) {
+    string str;
     const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     for (int i = 0; i < length; ++i) {
         str += charset[ rand() % (sizeof(charset) - 1)];
@@ -18,7 +18,7 @@ std::string generateRandomString(int length) {
 }
 
 // 测试String类的哈希值计算
-void HashCalculation(std::string elem,int count) {
+void HashCalculation(string elem,int count) {
     StringHashFamily hashFamily(count);  // 定义 count 个哈希函数
     cout << "Hashing '" << elem << "' with hash functions:" << endl;
     for (int i = 0; i < count; ++i) {
@@ -30,107 +30,76 @@ void HashCalculation(std::string elem,int count) {
 
 void test_stringHash()
 {
+    cout<< "****************************************"<< "\n";
+    cout<< "************* TEST STRING **************"<< "\n";
+    cout<< "****************************************"<< "\n" << endl;
     srand(time(0));  // 传入当前时间作为种子
-    HashTable<std::string,StringHashFamily> Table1(10,4);
+    HashTable<std::string,StringHashFamily> Table1(5,3);
     Table1.print_Functions();
     std::string str = "hello";
-    Table1.insert(str);
+    Table1.insert(str, true); /// debug=true，可视化步骤细节
+    Table1.print_Table();
     int same = 0;
-    for(int i=0;i<15;i++)
+    for(int i=0;i<12;i++)
     {
-        bool fail = Table1.insert(generateRandomString(5));
+        bool fail = Table1.insert(generateRandomString(5),true);/// debug=true，可视化步骤细节
         if (fail == false)
             same ++;
-        Table1.print_TableSize();
         Table1.print_Table();
+        Table1.print_TableSize();
         Table1.print_currentSize();
 
     }
-    cout<<Table1.contains("hello")<< endl;
+    cout<<"contain hello? " << Table1.contains("hello")<< endl;
     Table1.remove("hello");
-    Table1.print_currentSize();
     Table1.print_Table();
-    cout<<Table1.contains("hello")<< endl;
-    Table1.print_TableSize();
+    Table1.print_currentSize();
+    cout<<"contain hello? " << Table1.contains("hello")<< endl;
 
     cout << "same is "<< same << endl;
 }
 /////////////////////////////////// STRING HASH TEST ////////////////////////////////->end
 
+////////////////////////////////////// INT HASH TEST /////////////////////////////////////
+void test_intHash()
+{
+    cout<< "****************************************"<< "\n";
+    cout<< "*************** TEST INT ***************"<< "\n";
+    cout<< "****************************************"<< "\n" << endl;
+    srand(time(0));  // 传入当前时间作为种子
+    HashTable<int,IntHashFamily> Table2(5,3);
+    Table2.print_Functions();
+    Table2.print_Table();
+    Table2.insert(104133,true); /// debug=true，可视化步骤细节
+    Table2.print_Table();
+    int same = 0;
+    for(int i=1;i<12;i++)
+    {
+        ///  随机生成1000000以内的整数
+        bool fail = Table2.insert(abs(rand())%1000000,true); /// debug=true，可视化步骤细节
+        if (fail == false)
+            same ++;
+        Table2.print_Table();
+        Table2.print_TableSize();
+        Table2.print_currentSize();
 
+    }
+    cout<<"contain 104133? " << Table2.contains(104133)<< endl;
+    Table2.remove(104133);
+    Table2.print_Table();
+    Table2.print_currentSize();
+    cout<<"contain 104133? " << Table2.contains(104133)<< endl;
+
+    cout << "same is "<< same << endl;
+
+}
+////////////////////////////////////// INT HASH TEST ////////////////////////////////->end
 
 
 
 int main()
 {
-    srand(time(0));  // 传入当前时间作为种子
-    HashTable<std::string,StringHashFamily> Table1(10,4);
-    Table1.print_Functions();
-    std::string str = "hello";
-    Table1.insert(str);
-    int same = 0;
-    for(int i=0;i<15;i++)
-    {
-        string new_str  =generateRandomString(15);
-        bool fail = Table1.insert(new_str);
-        if (fail == false)
-            same ++;
-        Table1.print_TableSize();
-        Table1.print_Table();
-        Table1.print_currentSize();
-
-    }
-    cout<<Table1.contains("hello")<< endl;
-    Table1.remove("hello");
-    Table1.print_currentSize();
-    Table1.print_Table();
-    cout<<Table1.contains("hello")<< endl;
-    Table1.print_TableSize();
-
-    cout << "same is "<< same << endl;
-
-
+    test_stringHash();
+    test_intHash();
     return 0;
 }
-
-// int vectorSize = 10; // 向量的大小
-    // vector<string> vec(vectorSize);
-
-    // // 使用 generateRandomString 函数填充向量
-    // for (auto & elem : vec)
-    // {
-    //     elem = generateRandomString(5);
-    //     cout << elem << endl;
-    //     HashCalculation(elem,5);
-    // }
-// Table1.insert("hello");
-
-    // Table1.insert("goodbye");
-
-    // cout << Table1.contains("gello") << endl;
-
-
-// srand(time(0));  // 传入当前时间作为种子
-    // HashTable<std::string,StringHashFamily> Table1(200,4);
-    // Table1.print_Functions();
-    // std::string str = "hello";
-    // Table1.insert(str);
-    // int same = 0;
-    // for(int i=0;i<15;i++)
-    // {
-    //     bool fail = Table1.insert(generateRandomString(5));
-    //     if (fail == false)
-    //         same ++;
-    //     Table1.print_TableSize();
-    //     Table1.print_Table();
-    //     Table1.print_currentSize();
-
-    // }
-    // cout<<Table1.contains("hello")<< endl;
-    // Table1.remove("hello");
-    // Table1.print_currentSize();
-    // Table1.print_Table();
-    // cout<<Table1.contains("hello")<< endl;
-    // Table1.print_TableSize();
-
-    // cout << "same is "<< same << endl;
